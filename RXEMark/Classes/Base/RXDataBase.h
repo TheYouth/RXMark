f88289a9541a16d2b1daf5e818a5e50a9c4fd0ca
pingan <EX_WLJR_RENXINGXING@pingan.com.cn>
2018-09-11 15:09:06 +0800

    创建数据库类，根据路径获取数据库，获取某个表的版本号，更新表的版本号

--- RXEMark/Classes/Base/RXDataBase.h
+++ RXEMark/Classes/Base/RXDataBase.h
@@ -9,6 +9,7 @@
 #import <Foundation/Foundation.h>
 
 FOUNDATION_EXTERN NSInteger const kEMInvalidVersion;
+
 @class FMDatabase;
 @class FMDatabaseQueue;
 @interface RXDataBase : NSObject
@@ -16,6 +17,27 @@ FOUNDATION_EXTERN NSInteger const kEMInvalidVersion;
 @property (nonatomic, readonly) FMDatabaseQueue *dbQueue;
 @property (nonatomic, copy) NSString *tag;
 
+/**
+ 初始化
+ @param path 数据库存储路径
+ @return RXDataBase
+ */
+- (instancetype)initWithDBPath:(NSString *)path;
 
+/**
+ 获取某个表的版本号
+ @param tableName 表名
+ @return 版本号，没有表则返回默认值 kEMInvalidVersion(0)
+ */
+- (NSInteger)getVersionOfTable:(NSString *)tableName;
+
+/**
+ 更新某个表的版本号，正常的版本号应该是：version > 0
+
+ @param version 版本号
+ @param tableName 表名称
+ @return 是否更新成功
+ */
+- (BOOL)upadateVersion:(NSInteger)version ofTableName:(NSString *)tableName;
 
 @end
--- RXEMark/Classes/Base/RXDataBase.m
+++ RXEMark/Classes/Base/RXDataBase.m
@@ -7,7 +7,81 @@
 //
 
 #import "RXDataBase.h"
+#import "FMDB.h"
+
 NSInteger const kEMInvalidVersion = 0;
+
+@interface RXDataBase()
+@property (nonatomic) FMDatabaseQueue *dbQueue;
+
+//数据库内表的版本号
+@property (atomic, copy) NSDictionary <NSString*, NSNumber *> *tableVersionMap;
+
+
+@end
+
 @implementation RXDataBase
+
+- (instancetype)initWithDBPath:(NSString *)path{
+    if (self = [super init]) {
+        _dbQueue = [FMDatabaseQueue databaseQueueWithPath:path];
+        [self loadTableVersions];
+    }
+    return self;
+}
+- (void)loadTableVersions{
+    __weak __typeof(self) weakSelf = self;
+    [self.dbQueue inDatabase:^(FMDatabase *db) {
+        BOOL isSuccessed = [db executeUpdate:@"CREATE TABLE IF NOT EXISTS table_version ("
+                            @"name TEXT PRIMARY KEY, "
+                            @"version INTEGER DEAFULT 0);"];
+        if (isSuccessed) {
+            [db executeUpdate:@"CREATE INDEX IF NOT EXISTS index_name ON table_version(name);"];
+        }
+        FMResultSet *resultSet = [db executeQuery:@"SELECT * FROM table_version"];
+        NSMutableDictionary *versionsDict = [NSMutableDictionary dictionary];
+        while ([resultSet next]) {
+            NSString *tableName = [resultSet stringForColumn:@"name"];
+            NSInteger version = [resultSet intForColumn:@"version"];
+            if (tableName.length > 0) {
+                [versionsDict setObject:@(version) forKey:tableName];
+            }
+        }
+        [resultSet close];
+        weakSelf.tableVersionMap = versionsDict;
+    }];
+}
+- (NSInteger)getVersionOfTable:(NSString *)tableName{
+    NSNumber *versionNum = nil;
+    if (nil == self.tableVersionMap) {
+        return kEMInvalidVersion;
+    }
+    versionNum = [self.tableVersionMap objectForKey:tableName];
+    if (nil == versionNum) {
+        return kEMInvalidVersion;
+    }
+    return  [versionNum integerValue];
+}
+- (BOOL)upadateVersion:(NSInteger)version ofTableName:(NSString *)tableName{
+    if (tableName.length == 0) {
+        return NO;
+    }
+    NSAssert(version > 0, @"The version of table %@ should be bigger than 0!", tableName);
+    NSInteger currVersion = [self getVersionOfTable:tableName];
+    if (currVersion == version) {
+        return NO;
+    }
+    __block BOOL isSuccessed = NO;
+    __weak __typeof(self) weakSelf = self;
+    [self.dbQueue inDatabase:^(FMDatabase *db) {
+        isSuccessed = [db executeUpdate:@"INSERT OR REPLACE INTO table_versions (name,version) VALUES (?,?);",tableName,@(version)];
+        if (isSuccessed) {
+            NSMutableDictionary *versionDict = [NSMutableDictionary dictionary];
+            [versionDict setObject:@(version) forKey:tableName];
+            weakSelf.tableVersionMap = versionDict;
+        }
+    }];
+    return isSuccessed;
+}
 
 @end
