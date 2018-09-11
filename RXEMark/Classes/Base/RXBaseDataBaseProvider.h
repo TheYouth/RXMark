ecca661e7b7adea32175666e284bcdcf25dbd4e1
pingan <EX_WLJR_RENXINGXING@pingan.com.cn>
2018-09-11 15:09:37 +0800

    数据库关联类，创建表，更新表，关联表

--- RXEMark/Classes/Base/RXBaseDataBaseProvider.h
+++ RXEMark/Classes/Base/RXBaseDataBaseProvider.h
@@ -8,14 +8,32 @@
 
 #import "RXBaseDataProvider.h"
 #import "RXDataBase.h"
-@protocol EMBaseDataBaseProvider<RXBaseDataProvider>
+//接口表示子类需要重写的
+@protocol RXBaseDataBaseProvider<RXBaseDataProvider>
 
-- (BOOL)onCreateTable:()db;
+/**
+ 创建表
+
+ @param db 表
+ @return 是否成功
+ */
+- (BOOL)onCreateTable:(FMDatabase *)db;
+
+/**
+ 更新表
+
+ @param db 表
+ @return 是否更新成功
+ */
+- (BOOL)onUpgradeTable:(FMDatabase *)db fromVersion:(NSInteger)fromVersion toVersion:(NSInteger)toVersion;
 
 @end
 
-@interface RXBaseDataBaseProvider : RXBaseDataProvider
+@interface RXBaseDataBaseProvider : RXBaseDataProvider<RXBaseDataBaseProvider>
 
+@property (nonatomic, readonly) FMDatabaseQueue *dbQueue;
+
+- (void)buildTable:(RXDataBase *)dataBase;
 
 
 @end
--- RXEMark/Classes/Base/RXBaseDataBaseProvider.m
+++ RXEMark/Classes/Base/RXBaseDataBaseProvider.m
@@ -7,7 +7,56 @@
 //
 
 #import "RXBaseDataBaseProvider.h"
-
+#import "FMDB.h"
 @implementation RXBaseDataBaseProvider
 
+#pragma mark - Implemente EMBaseDataProvider
+
+- (NSString *)name{
+    NSAssert(NO, @"tableName should not be nil!");
+    return nil;
+}
+- (NSInteger)version{
+    return 1;
+}
+- (BOOL)onCreateTable:(FMDatabase *)db{
+    NSAssert(NO, @"SEL(onCreateTable:) should be override");
+    return NO;
+}
+
+- (BOOL)onUpgradeTable:(FMDatabase *)db fromVersion:(NSInteger)fromVersion toVersion:(NSInteger)toVersion{
+    NSAssert(NO,@"SEL(onUpgradeTable: fromVersion: toVersion:) should be override");
+    return NO;
+}
+- (FMDatabaseQueue *)dbQueue{
+    NSAssert(NO,@"SEL(dbQueue) should be override");
+    return nil;
+}
+#pragma mark - Public
+
+- (void)buildTable:(RXDataBase *)dataBase{
+    if ([NSThread isMainThread]) {
+        NSLog(@"Don,t build table in main thread!");
+    }
+    if (0 == self.name.length) {
+        return;
+    }
+    NSInteger version = [dataBase getVersionOfTable:self.name];
+    NSInteger latestVersion = [self version];
+    __block BOOL updatedVersion = NO;
+    if (version == kEMInvalidVersion) {
+        __weak __typeof(self) weakSelf = self;
+        [dataBase.dbQueue inDatabase:^(FMDatabase *db) {
+            updatedVersion = [weakSelf onUpgradeTable:db fromVersion:version toVersion:latestVersion];
+        }];
+    }else if (latestVersion > version){
+        __weak __typeof(self) weakSelf = self;
+        [dataBase.dbQueue inDatabase:^(FMDatabase *db) {
+            updatedVersion = [weakSelf onUpgradeTable:db fromVersion:version toVersion:latestVersion];
+        }];
+    }
+    if (updatedVersion) {
+        [dataBase upadateVersion:latestVersion ofTableName:self.name];
+    }
+}
 @end
