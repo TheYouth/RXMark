632c5bacaaee7d19419a57570b438a8f9ff98dae
pingan <EX_WLJR_RENXINGXING@pingan.com.cn>
2018-09-17 09:05:54 +0800

    添加日记模型并增加属性

--- RXEMark/Classes/Diary/Model/Bean/RXDiaryInfo.h
+++ RXEMark/Classes/Diary/Model/Bean/RXDiaryInfo.h
@@ -7,7 +7,15 @@
 //
 
 #import "RXAutoCoding.h"
-
+#import <UIKit/UIKit.h>
 @interface RXDiaryInfo : RXAutoCoding
+
+@property (nonatomic, assign) NSInteger  diaryId;
+@property (nonatomic, copy) NSString     *diaryDate;
+@property (nonatomic, copy) NSString     *diaryContent;
+@property (nonatomic, strong) UIImage    *diaryMiddleImage;
+@property (nonatomic, strong) UIImage    *diaryImage;
+
+
 
 @end
